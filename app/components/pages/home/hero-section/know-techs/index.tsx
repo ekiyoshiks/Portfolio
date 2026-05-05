6b62eda3954ee30041473cae8260030ef85c065e
Enzo Kiyoshi <kiyoshienzo@gmail.com>
2026-05-05 10:09:48 -0300

    Made Skills/competencies session + time skill

--- app/components/pages/home/hero-section/know-techs/index.tsx
+++ app/components/pages/home/hero-section/know-techs/index.tsx
@@ -1,12 +1,20 @@
 import { SectionTitle } from "@/app/components/section-title"
+import { KnowTech } from "./known-tech"
+import { TbBrandPython } from "react-icons/tb"
 
 export const KnowTechs = () => {
     return (
         <section className="container py-16">
             <SectionTitle subtitle="competencies" title="Skills" />
         
-            <div>
-                
+            <div className="mt-10 grid grid-cols-4 [repeat(auto-fit,minmax(264px,1fr))] sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+               {Array.from({ length: 8 }).map((_, index) => (
+                    <KnowTech key={index} tech={{
+                        icon: <TbBrandPython />,
+                        name: "Python",
+                        startDate: "2022-02-10"
+                    }} />
+                ))}
             </div>
         </section>
     )
--- app/components/pages/home/hero-section/know-techs/known-tech.tsx
+++ app/components/pages/home/hero-section/know-techs/known-tech.tsx
@@ -1,3 +1,4 @@
+import { getRelativeTimeString } from "@/app/utils/get-relative-time";
 import {ReactNode} from "react";
 
 type KnowTechProps = {
@@ -9,12 +10,16 @@ type KnowTechProps = {
 }
 
 export const KnowTech = ({ tech }: KnowTechProps) => {
+    const relativeTime = getRelativeTimeString(
+        new Date(tech.startDate), 'en-US').replace(' ago',' ').replace(' years', 'y')
     return (
-        <div>
-            <div>
-                <p>{tech.name}</p>
+        <div className="p-6 rounded-lg bg-gray-600/20 text-stone-500 flex flex-col gap-2 hover:text-stone-200 hover:bg-stone-700/30 transition-all">
+            <div className="flex items-center justify-between">
+                <p className="font-medium">{tech.name}</p>
                 {tech.icon}
             </div>
+
+            <span>{relativeTime} of experience</span>
         </div>
     )
 }
